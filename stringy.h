495a9fc5b7033ebe25228bf6d867694bb2f9fb9c
Helen Ye <00yhelen@gmail.com>
2017-10-02 10:34:26 -0400

    Add headers

---
+++ stringy.h
@@ -0,0 +1,6 @@
+int sterrlen( char * );
+char * sterncpy( char *dest, char *source, int n);
+char * sterncat( char *dest, char *source, int n)
+int sterrcmp( char *s1, char *s2 );
+char * sterrchr( char *s, char c );
+char * sterrsterr( char *s1, char * s2 );
