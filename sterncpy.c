9dce0382d658f29fd1b41bcac4cb05ea89fe0f3d
Helen Ye <00yhelen@gmail.com>
2017-10-02 11:56:31 -0400

    Minor fixes

--- sterncpy.c
+++ sterncpy.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "stringy.h"
 
 char * sterncpy( char *dest, char *source, int n) {
     int i = 0;
-    while(i < n && source[i]) {
+    while (i < n && source[i]) {
         dest[i] = source[i];
         i++;
     }
-    for(; i < n; i++) {
+    for (; i < n; i++) {
         dest[i] = '\0';
     }
     return dest;
@@ -23,9 +24,9 @@ int main() {
     printf("s1: %s\n", s1);
     printf("s2: %s\n", s2);
     sterncpy(s1, s, sizeof(s1) - 1);
-    printf("===after sterncpy===\n");
+    printf("===after sterncpy s into s1===\n");
     printf("s1: %s\n", s1);
-    printf("===after strncpy===\n");
+    printf("===after strncpy s into s2===\n");
     strncpy(s2, s, sizeof(s2) - 1);
     printf("s2: %s\n", s2);
 }
